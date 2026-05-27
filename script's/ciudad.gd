11e875bfdab1183b0bf0906f72095adcdbd3b613
vicente <vicente.valenzuela15@inacapmail.cl>
2026-05-27 18:51:51 -0400

    Add sign dialogue and rename ciudad script
    Add assets/assets-tiles-props/dialogues/cartel_1.dialogue and its .import, and register the new dialogue in project.godot translations. Rename tcnes/ciudad.gd to script's/ciudad.gd and update tcnes/ciudad.tscn to point to the new script path; adjust the script to play "nivel2/decoracion_xd". Also add an ext_resource for tcnes/interactuable.tscn and link the new dialogue resource in the ciudad scene. Minor update to tcnes/little_knigth.tscn.

--- script's/ciudad.gd
+++ script's/ciudad.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"nivel2/AnimatedSprite2D".play("default")
+	$"nivel2/decoracion_xd".play("default")
