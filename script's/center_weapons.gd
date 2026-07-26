cd58e1d2990cac94c4d8cb126f1d6db8baf91d0d
vicente <vicente.valenzuela15@inacapmail.cl>
2026-07-26 02:24:44 -0400

    Refactor weapon system and reorganize assets
    - Upgrade project to Godot 4.7
    - Implement WeaponData class for weapon property management
    - Reorganize weapons from little_knigth/weapons/ to dedicated weapons/ folder
    - Refactor center_weapons.gd: change from Area2D to Node2D, improve rotation/aiming logic, make swing animation configurable
    - Restructure weapon sprite hierarchy under hurtbox
    - Add new test scenes (menu_pausa.tscn, test_ground.tscn)
    - Update animation loop format (true -> 1) for Godot 4.7 compatibility

--- script's/center_weapons.gd
+++ script's/center_weapons.gd
@@ -1,52 +1,74 @@
-extends Area2D
+extends Node2D  # center-weapons
 
-#con estos dos contramos el flujo principal
+class WeaponData:
+	var name: String
+	var texture: Texture2D
+	var damage: int
+	var swing_range: float
+	var swing_speed: float
+	func _init(n, tex, dmg, range_deg, speed):
+		name = n
+		texture = tex
+		damage = dmg
+		swing_range = deg_to_rad(range_deg)
+		swing_speed = speed
+
+var weapons: Array[WeaponData] = []
+var current_weapon_index = 0
+
+@onready var hurtbox = $hurtbox
+@onready var sword_sprite = $"hurtbox/sword-swing"
+
 var attacking = false
 var locked_rotation = 0.0
 
+func _ready():
+	# ejemplo, ajusta las rutas a tus texturas reales
+	weapons.append(WeaponData.new("Espada", preload("res://weapons/001.png"), 10, 120, 10.0))
+	#weapons.append(WeaponData.new("Hacha", preload("res://sprites/axe.png"), 18, 90, 6.0))
+	#weapons.append(WeaponData.new("Daga", preload("res://sprites/dagger.png"), 5, 150, 16.0))
+	equip(0)
+	$hurtbox/CollisionShape2D.disabled = true
+	hurtbox.hide()
 
-func _ready() -> void:
-	hide()
+func equip(index: int):
+	current_weapon_index = index
+	var w = weapons[index]
+	sword_sprite.texture = w.texture
+
+func current_weapon() -> WeaponData:
+	return weapons[current_weapon_index]
+
+
+#func _unhandled_input(event):
+#	if event.is_action_pressed("next_weapon"):
+#		equip((current_weapon_index + 1) % weapons.size())
+#	elif event.is_action_pressed("prev_weapon"):
+#		equip((current_weapon_index - 1 + weapons.size()) % weapons.size())
 
 func _process(_delta):
 	if not attacking:
-		look_at(get_global_mouse_position()) #look at gira el marker a donde esta el raton y el get global da la posicion del raton
-		rotation += deg_to_rad(45)
-	if Input.is_action_just_pressed("hit") and not attacking:
-		attack()
-
-
-
+		rotation = (get_global_mouse_position() - global_position).angle()
+		if Input.is_action_just_pressed("hit"):
+			attack()
 
 func attack():
 	attacking = true
-	
-	# le asignamos el valor para que no se mueva mas
+	var w = current_weapon()
 	locked_rotation = rotation
-	
-	# activamos la hitbox
 	$hurtbox/CollisionShape2D.disabled = false
-	show()
-	
-	# esperamos a que termine el swing
-	await swing()
-
-	# desactivas
+	hurtbox.show()
+	await swing(w.swing_range, w.swing_speed)
 	$hurtbox/CollisionShape2D.disabled = true
-	hide()
+	hurtbox.hide()
 	attacking = false
 
-
-func swing():
-	
-	var start = locked_rotation - deg_to_rad(60)
-	var end = locked_rotation + deg_to_rad(60)
-	
+func swing(range_rad: float, speed: float):
+	var start = locked_rotation - range_rad / 2
+	var end = locked_rotation + range_rad / 2
 	rotation = start
-	"""get_process_delta_time() devuelve el tiempo pasado entre frames por ejemplo
-	0,16 * 8 y si aumentamos el * 8 le damos mas velocidad al swing"""
 	var t = 0.0
 	while t < 1.0:
-		t += get_process_delta_time() * 8 
+		t += get_process_delta_time() * speed
 		rotation = lerp(start, end, t)
 		await get_tree().process_frame
